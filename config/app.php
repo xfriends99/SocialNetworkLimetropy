96bc374bc764a3ec4c821db8fe142ab2e5686b0d
Javier Palma <jpalma@theegg.io>
2018-05-07 06:22:19 -0400

    Add laravel Corsd

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         /*
          * Application Service Providers...
          */
+	Barryvdh\Cors\ServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
